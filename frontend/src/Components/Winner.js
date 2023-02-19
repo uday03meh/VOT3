1a24894b5d9b418370f498ecfd9837b8ba21b901
Uday Mehtani <udaymehtani@gmail.com>
2023-02-19 20:55:24 +0530

    edited css

--- frontend/src/Components/Winner.js
+++ frontend/src/Components/Winner.js
@@ -20,8 +20,8 @@ import {useState,useEffect} from "react"
         <div className="flex flex-col gap-4 items-center justify-center">
           <button onClick={declareWinner} className="bg-blue-500 text-white px-4 py-2 rounded-lg flex items-center justify-center">Declare Winner</button>
           <div className="text-4xl font-bold">{winner}</div>
-          <img src={`${winner.imageURL}`} className="w-40 h-40 rounded-full" alt = {winner}/>
-          <img src="https://pbs.twimg.com/profile_images/1609351112272007168/onsxRv3O_400x400.jpg" className="w-40 h-40 rounded-full" alt = {winner}/>
+          {/* <img src={`${winner.imageURL}`} className="gw-40 h-40 rounded-full" alt = {winner}/> */}
+          {/* <img src="https://pbs.twimg.com/profile_images/1609351112272007168/onsxRv3O_400x400.jpg" className="w-40 h-40 rounded-full" alt = {winner}/> */}
         </div>
       );
       }
